e15abd0b70ddef76e30ea29ac0169f395170a909
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-09 23:56:49 +0500

    update

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -1,12 +1,3 @@
-// const obj = {
-//     name : "javohir", 
-//     address : {
-//         town : "samarqand",
-//     }
-// }
-
-function myFunction(){
-    console.log(1)
+function heyhello(){
+    
 }
-const arr =  [1, 2, 3]
-console.log(typeof(myFunction))
