f5b1a935da0b4d24d85e71da2b5e6b7b0b1b445c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-10 03:31:03 +0500

    update

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -1,23 +1,10 @@
-const done =  true
+const myMethods = {
+    printName(age){
+        console.log(this.name , age)
+    }
+}
 
-const  promise1 = new Promise((resolve , reject)=>{
-    if(done)  resolve("done1")
-    else reject('error1')
-})
-const  promise2 = new Promise((resolve , reject)=>{
-    if(!done)  resolve("done2")
-    else reject('error2')
-})
-const  promise3 = new Promise((resolve , reject)=>{
-    if(done) resolve("done3")
-    else reject('error3')
-})
+const emp1 =  {name : 'javohir'}
 
-
-const all =  Promise.race([promise1 , promise2 , promise3])
-
-all.then(res=>{
-    console.log(res)
-}).catch(err=>{
-    console.log(err)
-})
+const pand = myMethods.printName.bind(emp1 , 21)
+pand()
