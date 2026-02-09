2d5f2d3e0b761c7dcd84306bd231ec544d006a28
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-10 01:52:05 +0500

    update

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -1,1 +1,23 @@
-console.log(0 ?? 10)
+const metMethods = {
+    printMyname() {
+        console.log(this.name)
+    },
+
+    printMyAge() {
+        console.log(this.age)
+    },
+
+    printFullname(surname){
+        console.log(this.name , surname)
+    }
+}
+
+const emp1 = { name: 'javohir', age: 21 }
+const emp2 = { name: 'jalol', age: 20 }
+
+// metMethods.printMyname.call(emp1)
+// metMethods.printMyname.call(emp2)
+// metMethods.printMyAge.call(emp1)
+// metMethods.printMyAge.call(emp2)
+// metMethods.printFullname.call(emp1 , 'suvonov')
+// metMethods.printFullname.call(emp2 , 'toshtemirov')
