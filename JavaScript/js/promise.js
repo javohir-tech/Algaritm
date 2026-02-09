29eb5a63b40e689bb61749655bff870be7e5eaa8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-10 00:01:21 +0500

    update

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -1,3 +1,16 @@
-function heyhello(){
-    
+////// first classs function
+
+const res = heyhello()
+console.log(res)
+
+function greet(helloFunc , name){
+    console.log(`${helloFunc()} ${name}`)
 }
+
+greet(heyhello , 'javohir')
+
+function heyhello(){
+    return "hello"
+}
+
+
