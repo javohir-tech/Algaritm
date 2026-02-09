904135ca27826a97d46f024794214020e4ba0515
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-09 23:32:55 +0500

    update

---
+++ JavaScript/js/promise.js
@@ -0,0 +1,12 @@
+// const obj = {
+//     name : "javohir", 
+//     address : {
+//         town : "samarqand",
+//     }
+// }
+
+function myFunction(){
+    console.log(1)
+}
+const arr =  [1, 2, 3]
+console.log(typeof(myFunction))
