e44a511b55a5bb8400922ee0759563347330b69b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-10 00:56:12 +0500

    update

--- JavaScript/js/promise.js
+++ JavaScript/js/promise.js
@@ -1,3 +1,19 @@
-const test= ['a' , 'b' , 'c']
+const arr = [1, 2, 4, 5, 10, 13, 24, 27]
 
-console.log(test.includes('a'))
+// const res = arr.find(a => a > 10)
+// const res = arr.filter(a => a <= 10)
+// const res = arr.findIndex(a => a > 10)
+// const res = arr.map(item => item*2)
+// const res = arr.map(item => item*2)
+// const text = "salom dunyo"
+// const res = text.split(" ")
+// const res2 = res.join(" ")
+
+// console.log(res)
+// console.log(res2)
+// console.log(arr.reverse())
+
+const res = arr.every(item => item > 10)
+console.log(res)
+
+
