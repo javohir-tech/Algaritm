5756f4ad9da086077e21e4ba5cb247c8fab491c3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-03-27 23:34:44 +0500

    update

---
+++ JavaScript/VUE/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue';
+
+const user = reactive({
+  age: 21
+})
+
+const increment = () => {
+  user.age ++
+}
+</script>
+
+<template>
+  <header>
+    VUE learn
+  </header>
+  <p>User age {{ user.age }}</p>
+  <button @click="increment">increment</button>
+  <RouterView />
+</template>
+
+<style scoped></style>
