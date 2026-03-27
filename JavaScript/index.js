3b2b6a1adc2062f2845a28c70ce4a89945230b61
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-03-28 01:30:59 +0500

    clousere

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,22 @@
 const obj = {
-  name: "Javohir",
-  greet: function() {
-    console.log(this.name);
-  }
+    name: "Javohir",
+    greet: function () {
+        console.log(this.name);
+    }
 }
 
 const fn = obj.greet;
 fn.call(obj)
+
+function makeCounter (){
+    let count = 0
+    return function (){
+        count++
+        return count
+    }
+}
+
+const counter = makeCounter()
+console.log(counter())
+console.log(counter())
+console.log(counter())
