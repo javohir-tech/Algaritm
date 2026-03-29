e62264127e56611751da70c1e86bc2d315e1eab3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-03-29 14:13:59 +0500

    void'

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,27 +1,15 @@
 "use strict";
+////////// VOID ///////////////////
 Object.defineProperty(exports, "__esModule", { value: true });
-const persons = [
-    {
-        firstName: "Javohir",
-        lastName: "Suvonov",
-        age: 21,
-        postion: "full Stack developer",
-        // hoppy: "play  piano",
-        // cource: 4,
-        expirianse: 2,
-        coupuse: "east",
-        greet: function (message) {
-            return message;
-        },
-        sayHello: function (name) {
-            return `Hello ${name}`;
-        }
-    },
-];
-const person1 = persons[0];
-console.log(person1?.greet("salom"));
-console.log(person1?.sayHello("Javohir"));
-// person1!.firstName = "suvonov"
-console.log(person1?.firstName);
-console.log(persons);
+// function myFunction (text : string){
+//   console.log(text)
+// }
+// myFunction("hello")
+function greet(text, callback) {
+    console.log(text);
+    callback(`logget : ${text}`);
+}
+greet("hello", (message) => {
+    console.log(message);
+});
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,53 +1,16 @@
-////////////////// interface ////////////////////
-type seyHello = (name: string) => string;
+////////// VOID ///////////////////
 
-interface Person {
-  readonly firstName: string;
-  lastName: string;
-  age: number;
-  cource ?: 1 | 2 | 3 | 4;
-
-  greet: (message: string) => string;
-}
-
-// interface Person {
-//   hoppy: string;
+// function myFunction (text : string){
+//   console.log(text)
 // }
 
-interface Worker {
-  postion: string;
-  sayHello : seyHello
+// myFunction("hello")
+
+function greet(text : string , callback : (message : string) => void){
+  console.log(text)
+  callback(`logget : ${text}`)
 }
 
-interface Componyworker extends Person, Worker {
-  expirianse: number;
-  coupuse : "east" | "west"
-}
-
-const persons: Componyworker[] = [
-  {
-    firstName: "Javohir",
-    lastName: "Suvonov",
-    age: 21,
-    postion: "full Stack developer",
-    // hoppy: "play  piano",
-    // cource: 4,
-    expirianse: 2,
-    coupuse : "east",
-
-    greet: function (message) {
-      return message;
-    },
-
-    sayHello : function(name){
-      return `Hello ${name}`
-    }
-  },
-];
-
-const person1 = persons[0];
-console.log(person1?.greet("salom"));
-console.log(person1?.sayHello("Javohir"))
-// person1!.firstName = "suvonov"
-console.log(person1?.firstName)
-console.log(persons);
+greet("hello" , (message) =>{
+  return message
+})
