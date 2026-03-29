33d5e53555203086ecb7694ac52be2f5b8e401ac
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-03-29 23:23:53 +0500

    ys in vue

--- TypeScript/VUE/src/Pages/UsersPage.vue
+++ TypeScript/VUE/src/Pages/UsersPage.vue
@@ -6,10 +6,8 @@
     <div v-if="!loading && err">
         {{ err }}
     </div>
-    <div v-for="user in data" style="border: 1px solid red;">
-        <p>{{ user.name }}</p>
-        <p>{{ user.username }}</p>
-        <p>{{ user.address.city }}</p>
+    <div v-for="user in data ?? []" style="border: 1px solid red;" :key="user.id">
+        <UserCard :name="user.name" :username="user.username" :address="user.address" />
     </div>
 </template>
 
@@ -17,6 +15,7 @@
 import useFetch from '@/Hooks/useFetch'
 import { onMounted } from 'vue'
 import type { User } from '@/Types'
+import UserCard from '@/components/UserCard.vue'
 
 const { loading, data, err, getData } = useFetch<User[]>("https://jsonplaceholder.typicode.com/users")
 
---
+++ TypeScript/VUE/src/components/UserCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { User } from '@/Types';
+const props = defineProps<User>()
+</script>
+
+<template>
+   <p>{{ props.name }}</p>
+   <p>{{ props.username }}</p>
+   <p>{{ props.phone }}</p>
+</template>
+
+<style scoped>
+
+</style>
