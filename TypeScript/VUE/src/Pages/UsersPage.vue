e0b20a919b17d8a46a8ac570bc7d64f6d56a9eb3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-03-29 23:43:11 +0500

    tss in vue

--- TypeScript/VUE/src/Pages/UsersPage.vue
+++ TypeScript/VUE/src/Pages/UsersPage.vue
@@ -8,7 +8,7 @@
     </div>
     <div v-for="user in data ?? []" style="border: 1px solid red;" :key="user.id">
         <UserCard :id="user.id" :name="user.name" :username="user.username" :address="user.address" :phone="user.phone"
-            :website="user.website" :company="user.company" />
+            :website="user.website" :company="user.company" @delete="handleDelete" />
     </div>
 </template>
 
@@ -17,6 +17,12 @@ import useFetch from '@/Hooks/useFetch'
 import { onMounted } from 'vue'
 import type { User } from '@/Types'
 import UserCard from '@/components/UserCard.vue'
+
+const handleDelete = (id: number): void => {
+    if (!data.value) return
+    
+    data.value = data.value?.filter(c => c.id !== id)
+}
 
 const { loading, data, err, getData } = useFetch<User[]>("https://jsonplaceholder.typicode.com/users")
 
--- TypeScript/VUE/src/components/UserCard.vue
+++ TypeScript/VUE/src/components/UserCard.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import type { User } from '@/Types';
 const props = defineProps<User>()
+
+const emit = defineEmits<{
+   (e: "delete", value: number): void
+}>()
+
+const handleDelete = (id: number): void => {
+   emit("delete", id)
+}
+
 </script>
 
 <template>
    <p>{{ props.name }}</p>
    <p>{{ props.username }}</p>
    <p>{{ props.phone }}</p>
+   <button @click="handleDelete(props.id)">Ochirish</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
