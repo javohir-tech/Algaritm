87cf2a446331ad851a537a0fe5cd794faeb3c125
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-01 17:56:58 +0500

    update

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,3 +1,37 @@
-let a = "salom dunyo"
-
-console.log(a.toFixed(2))
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Myfunction(value) {
+    return value;
+}
+console.log(Myfunction(5));
+// const arr: (number | string)[] = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
+// const users: (IUser & {isMarried :  boolean})[] = [
+//   {
+//     name: "Javohir",
+//     age: 21,
+//     isMarried : false
+//   },
+// ];
+const users = [
+    {
+        name: "Javohir",
+        age: 21,
+        isMarried: false,
+    },
+];
+console.log(users);
+var Person;
+(function (Person) {
+    Person[Person["ADMIN"] = 0] = "ADMIN";
+    Person[Person["MANAGER"] = 1] = "MANAGER";
+    Person[Person["USER"] = 2] = "USER";
+})(Person || (Person = {}));
+const companyusers = [
+    {
+        position: Person.ADMIN,
+        age: 21,
+    },
+];
+console.log(companyusers);
+//# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,3 +1,48 @@
-let a : number = 2
+function Myfunction<T>(value: T): T {
+  return value;
+}
 
-console.log(a.toFixed(2))
+console.log(Myfunction<number>(5));
+
+// const arr: (number | string)[] = [1, 2, 3, 4, 5];
+const arr: Array<number> = [1, 2, 3, 4, 5];
+
+interface IUser {
+  name: string;
+  age: number;
+}
+
+// const users: (IUser & {isMarried :  boolean})[] = [
+//   {
+//     name: "Javohir",
+//     age: 21,
+//     isMarried : false
+//   },
+// ];
+
+const users: Array<IUser & { isMarried: false }> = [
+  {
+    name: "Javohir",
+    age: 21,
+    isMarried: false,
+  },
+];
+
+console.log(users);
+
+enum Person {
+  ADMIN,
+  MANAGER,
+  USER,
+}
+
+type USER = Record<string, number>;
+
+const companyusers: USER[] = [
+  {
+    position: Person.ADMIN,
+    age: 21,
+  },
+];
+
+console.log(companyusers)
