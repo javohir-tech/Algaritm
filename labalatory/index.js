4c9883de0240135fd6443543052eff2743a5da96
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-04-14 00:48:24 +0500

    updtaae

--- labalatory/index.js
+++ labalatory/index.js
@@ -1,13 +1,21 @@
-const ul = document.querySelector("ul")
+// const ul = document.querySelector("ul")
 
-ul.addEventListener("click", function(event) {
-    if (event.target.tagName === "LI") {
-        console.log("Clicekd :" , event.target.textContent )
-    }
-});
+// ul.addEventListener("click", function(event) {
+//     if (event.target.tagName === "LI") {
+//         console.log("Clicekd :" , event.target.textContent )
+//     }
+// });
 
 // var a = "i am in global"
 // console.log(window.a)
 
 // let b = "i am not in global"
 // console.log(window.b)
+
+console.log(Number(123))
+console.log(Number("123"))
+console.log(Number(true))
+console.log(Number(false))
+console.log(Number(null))
+console.log(Number(undefined))
+console.log(Number([1 ,  3]))
