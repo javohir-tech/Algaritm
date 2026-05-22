edec9a1b963c11b9c9ceff119861297a422ad048
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-22 23:19:44 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,2 +1,16 @@
-name = "Anvar"
-print("Anvar")
+name = "javohir"
+surname = "Suvonov"
+full_name = f"{name} \n{surname}"
+
+# print(full_name.lower())
+# print(full_name.upper())
+# print(full_name.title())
+# print(full_name.capitalize())
+
+meva = "                  olma                    "
+# print(meva.lstrip())
+# print(meva.rstrip())
+# print(meva.strip())
+
+ism = input("Ismingiz nima?\n>>>")
+print(f"Assalom aleykum {ism.title()}!!!")
