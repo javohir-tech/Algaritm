9f10abd9b6cb67290ab692d7f4f941c281187b8a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-23 23:17:19 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,12 +1,1 @@
-a = 20 # int
-b = 6.2 # float 
-print(type(a))
-print(type(b))
-
-people = 7_875_4563_456
-print(people)
-
-print(int(a*b))
-print(100/2)
-print(int(10.1))
-print(str(10))
+print(101//2)
