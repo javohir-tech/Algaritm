2f9a93525f0894de9f76c4580510f7fb59769a55
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-25 23:52:52 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -3,3 +3,9 @@ cars = ["audi", "mers", "bmw", "bugatti", "porsche"]
 # numbers = range(0, 10)
 # number_list = list(numbers)
 # print(number_list)
+
+# toq_sonlar = list(range(1 , 20 , 2))
+# print(toq_sonlar)
+
+# juft_sonlar = list(range(0 , 20 , 2))
+# print(juft_sonlar)
