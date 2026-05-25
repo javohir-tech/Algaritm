4b964cd666d3375701c73327dd81527fc9f82095
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-26 00:00:54 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -9,3 +9,26 @@ cars = ["audi", "mers", "bmw", "bugatti", "porsche"]
 
 # juft_sonlar = list(range(0 , 20 , 2))
 # print(juft_sonlar)
+
+numbers = [1, 2, 3, 4, 5, 6]
+# print(numbers[0:4])
+
+# numbers2 = numbers[2:4]
+# print(numbers2)
+# print(numbers)
+
+# print(numbers[:3])
+# print(numbers[2:])
+
+# pythonda  ham masssivlar qiymat emas reference saqlaydi 
+
+# my_cars = cars
+# my_cars = cars[:] # js dagi ...arr spreed operatori kabi 
+# my_cars.append("labarchini")
+# my_cars.remove("bmw")
+# print(my_cars)
+# print(cars)
+
+
+
+
