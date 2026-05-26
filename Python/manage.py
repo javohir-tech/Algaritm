5b8e33cd2bd4edc8a63136f3015d163dc8faabba
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-26 23:06:58 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,3 +1,24 @@
-print("hello world"
-      
-print("salom")
+#  nameError 
+# mevalar = ["olma", "meva", "nok"]
+# for meva in mvlar :
+    # print(meva)
+
+# valueError
+# agar float son kiritilsa int() methodi ValueError beradi 
+# son = int(input("istalgan sonni kiritinglar !!! >>>"))
+# if son >= 0:
+#     print("Musbat son")
+# else : 
+#     print("Manfiy sonlar")
+
+#ZeroDivisionError
+# x , y = 50 , 50
+# print(250/(x-y))
+
+# mantiqiy Errorlar 
+# PI = 4.14
+# radius = 5
+# yuza = PI*radius**2
+# print(yuza)
+
+
