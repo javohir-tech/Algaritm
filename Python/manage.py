350fe27ed818f7e7df00434c7b3710d61f5dbfeb
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-28 11:08:08 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -3,3 +3,11 @@
 car_0 = {"model": "ferrari", "color": "red"}
 print(car_0["color"])
 print(car_0["model"])
+car_0["year"] = 1988
+print(car_0["year"])
+
+car_0['model'] = "porsche"
+print(car_0)
+
+del car_0['color']
+print(car_0)
