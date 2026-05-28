484bb6593b187a38dcabadacfd9f6e83dcc4eda4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-28 11:12:19 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,13 +1,6 @@
-# DICT
+mevalar = {"apple": "olma", "cherry": "olcha", "banana": "banan"}
 
-car_0 = {"model": "ferrari", "color": "red"}
-print(car_0["color"])
-print(car_0["model"])
-car_0["year"] = 1988
-print(car_0["year"])
-
-car_0['model'] = "porsche"
-print(car_0)
-
-del car_0['color']
-print(car_0)
+# print(mevalar['apricot'])
+# meva = mevalar.get("apricot" , "bunday meva mavjud emas")
+meva = mevalar.get("apricot" ) # None
+print(meva)
