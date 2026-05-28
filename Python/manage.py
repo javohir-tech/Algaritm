3c08761a5e7b0c2f2a0b369379bc035a78f952bc
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-28 11:03:38 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,24 +1,5 @@
-#  nameError 
-# mevalar = ["olma", "meva", "nok"]
-# for meva in mvlar :
-    # print(meva)
+# DICT
 
-# valueError
-# agar float son kiritilsa int() methodi ValueError beradi 
-# son = int(input("istalgan sonni kiritinglar !!! >>>"))
-# if son >= 0:
-#     print("Musbat son")
-# else : 
-#     print("Manfiy sonlar")
-
-#ZeroDivisionError
-# x , y = 50 , 50
-# print(250/(x-y))
-
-# mantiqiy Errorlar 
-# PI = 4.14
-# radius = 5
-# yuza = PI*radius**2
-# print(yuza)
-
-
+car_0 = {"model": "ferrari", "color": "red"}
+print(car_0["color"])
+print(car_0["model"])
