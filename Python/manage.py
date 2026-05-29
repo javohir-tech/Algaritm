5ade597770a7b4fb3b26200243af40a857efb9af
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-29 22:15:15 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,13 +1,27 @@
 # lugat bilan ishlash
 
 telefonlaar = {
-    "ali" : "mi 9" , 
-    "vali" : "iphone x",    
-    "umar" : "samsung 21", 
-    "sabi"  : "samsung 32"
+    "ali": "mi 9",
+    "vali": "iphone x",
+    "umar": "samsung 21",
+    "sabi": "samsung 32",
 }
 
-for key , value in telefonlaar.items():
-    print(f"Kalit : {key}")
-    print(f"Qiymat : {value}")
+# for key, value in telefonlaar.items():
+    # print(f"Kalit : {key}")
+    # print(f"Qiymat : {value}")
+
+mahsulotlar = {"olma": 10000, "nok": 25000, "cola": 12000, "snikers": 9000}
+
+bozorlik = ["olma", "nok"]
+
+# for mahsulot in mahsulotlar.keys():
+#     print(mahsulot)
     
+# key() shart emas kalitlarni olisuh uchun    
+# for mahsulot in mahsulotlar:
+#     print(mahsulot)
+
+
+
+
