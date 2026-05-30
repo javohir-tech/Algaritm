5c80ef24d955e08ba2bb902a693536b73c6924a6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-05-30 23:36:31 +0500

    update

---
+++ JavaScript/twoSum.js
@@ -0,0 +1,16 @@
+// let car = {
+//     model : "nexia", 
+//     year : 2015
+// }
+
+// let car2 = car
+
+// car2.model = "cobalt"
+// console.log(car)
+
+let a = 4 ;
+let b = a
+
+b++
+
+console.log(a)
