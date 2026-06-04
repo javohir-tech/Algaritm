ed2d4fc1fd35482591290ec8ded4634b3df6157a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-04 23:25:45 +0500

    update

--- Python/manage.py
+++
@@ -1,9 +0,0 @@
-def myFunction(ism : str)->str:
-    """ FUnksiya haqida ma'lumot """
-    return ism
-
-# print(myFunction("salom"))
-print(myFunction("javohir"))
-info = myFunction.__doc__
-# print(myFunction.__doc__)
-print(info)
