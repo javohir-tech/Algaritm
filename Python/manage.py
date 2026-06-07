08aa20e673311550c7bc7f21955e84e25e22fd31
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-07 21:35:31 +0500

    update

--- Python/manage.py
+++ Python/manage.py
@@ -1,1 +1,17 @@
-name = "javohir"
+# def summa(*sonlar):
+#     yigindi = 0
+#     for son in sonlar:
+#         yigindi += son
+#     return yigindi
+
+
+# print(summa(1, 2, 3, 5))
+
+
+# error sabab funksiyaga uzatilgan argumantlar tuple bolip  ishlaydi 
+def myFunction(*sonlar):
+    sonlar.push(2)
+    return sonlar
+
+
+print(myFunction(1, 2, 3, 4))
