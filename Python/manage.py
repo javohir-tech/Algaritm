06b7fc4cdaf14e43730985c949b5364b42dff79d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-08 22:02:59 +0500

    updare

--- Python/manage.py
+++
@@ -1,6 +0,0 @@
-def avto_info(kompaniya :str , model:str , **kwargs):
-    kwargs['kompaniya'] = kompaniya
-    kwargs['model'] = model
-    return kwargs
-
-print(avto_info("porche" , "911" , year = 1988 , color = "red" , km = 1234))
