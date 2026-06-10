5fb0f8dea4a057fe9f9106f5cb28fea1623e8503
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-10 16:37:17 +0500

    update

--- JavaScript/dfc.js
+++ JavaScript/dfc.js
@@ -1,17 +1,22 @@
-function TreeNode(val, left, rigth) {
-    this.val = val === undefined ? 0 : val
-    this.left = left === undefined ? 0 : left
-    this.rigth = rigth === undefined ? 0 : rigth
-    return [this.val, this.left, this.rigth]
-}
-
-function inorderTraversal(root) {
-    for (let i = 0; i < root.length; i++) {
-        res = TreeNode(root[i], root[i * 2 + 1], root[i * 2 + 2])
-        console.log(res)
+var inorderTraversal = function(root) {
+    const result = [];
+    const stack = [];
+    let current = root;
+    
+    while (current !== null || stack.length > 0) {
+        // Eng chapga bor
+        while (current !== null) {
+            stack.push(current);
+            current = current.left;
+        }
+        
+        current = stack.pop();
+        result.push(current.val); // O'rtani qo'sh
+        current = current.right;  // O'ngga o't
     }
-}
+    
+    return result;
+};
 
-
-root = [1, null, 2, 3]
-inorderTraversal(root)
+const root = [1, null, 2, 3]
+console.log(inorderTraversal(root))
