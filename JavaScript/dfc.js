5def9dd600ba5b4f7af2488f384bdef36028eac7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-11 11:52:50 +0500

    update

--- JavaScript/dfc.js
+++ JavaScript/dfc.js
@@ -1,22 +1,18 @@
-var inorderTraversal = function(root) {
-    const result = [];
-    const stack = [];
-    let current = root;
-    
-    while (current !== null || stack.length > 0) {
-        // Eng chapga bor
-        while (current !== null) {
-            stack.push(current);
-            current = current.left;
-        }
-        
-        current = stack.pop();
-        result.push(current.val); // O'rtani qo'sh
-        current = current.right;  // O'ngga o't
-    }
-    
-    return result;
-};
+function inorderTraversal(root) {
+    let result = [];
 
-const root = [1, null, 2, 3]
+    function dfs(node) {
+        if (!node) return;
+
+        dfs(node.left);   // chap
+        result.push(node.val); // root
+        dfs(node.right);  // o'ng
+    }
+
+    dfs(root);
+
+    return result;
+}
+
+const root = [1 , null , 2, 3]
 console.log(inorderTraversal(root))
