2399c7a5da396170d45fe00780d37afed4c993c5
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-19 23:16:41 +0500

    update

--- JavaScript/digits.js
+++ JavaScript/digits.js
@@ -1,22 +1,23 @@
-function addDigits(num) {
-    if (num % 10 === num) return num
-    const numbers = []
-
-    while (num !== 0) {
-        let temp_num = num % 10
-        numbers.push(temp_num)
-        num = (num - temp_num) / 10
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+    let sum = 0
+    while (num / 10 !== 0) {
+        let last_num = num % 10
+        sum += last_num
+        num = (num - last_num)/10
     }
 
-    const sum = numbers.reduce((a, b) => a + b, 0)
     if (sum % 10 !== sum) {
         return addDigits(sum)
     }
 
     return sum
-}
+};
 
-const num = 38234
+
+const num = 38
+
 console.log(addDigits(num))
-
-// console.log(387%100)
