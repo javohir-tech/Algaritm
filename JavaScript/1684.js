0e63475cda79dc1ec26f2781018a032ea0fdbd5e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-24 23:33:02 +0500

    update

---
+++ JavaScript/1684.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} allowed
+ * @param {stirng[]} words
+ * @return {number}
+*/
+
+function countConsistentStrings(allowed, words) {
+    const allowed_arr = allowed.split("")
+    for (let i = 0; i < allowed_arr.length; i++) {
+        
+    }
+}
+
+
+let allowed = "ab", words = ["ad", "bd", "aaab", "baa", "badab"]
+
+// console.log(countConsistentStrings(allowed, words))
+
+console.log("asd".split("").findIndex(a => a === "b"))
