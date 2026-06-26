8b4fa545c5331ba470d8de3798378ecd5a27db8e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-26 20:34:48 +0500

    update

---
+++ JavaScript/destCity.js
@@ -0,0 +1,14 @@
+/**
+ * @param  {string[][]} paths
+ * @return {string}
+*/
+
+function destCity(paths) {
+    const cities = paths.flat()
+    const now_city = cities[0]
+    
+}
+
+let paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]
+
+console.log(destCity(paths))
