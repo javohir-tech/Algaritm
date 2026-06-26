e7001f6a114bdb940926e53c50809d115ade32b6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-26 10:55:11 +0500

    updaye

--- JavaScript/1684.js
+++ JavaScript/1684.js
@@ -5,17 +5,28 @@
 */
 
 function countConsistentStrings(allowed, words) {
-    const allowed_arr = allowed.split("")
-    for (let i = 0; i < allowed_arr.length; i++) {
-        for (let j = 0; j < words.length; j++) {
-
+    let sum = 0
+    let arr_allowed = allowed.split("")
+    for (let i = 0; i < words.length; i++) {
+        let word_arr = words[i].split("")
+        let she_said_yes = true
+        for (let j = 0; j < word_arr.length; j++) {
+            if (arr_allowed.findIndex(a => a === word_arr[j]) === -1) {
+                she_said_yes = false
+            }
         }
+
+        if (she_said_yes) {
+            sum++
+        }
+
     }
+    return sum
 }
 
 
 let allowed = "ab", words = ["ad", "bd", "aaab", "baa", "badab"]
 
-// console.log(countConsistentStrings(allowed, words))
+console.log(countConsistentStrings(allowed, words))
 
-console.log("asd".split("").findIndex(a => a === "b"))
+// console.log("asd".split("").findIndex(a => a === "b"))
