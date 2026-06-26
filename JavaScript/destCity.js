c89502287d2a9f46e795f77d362273b42a2dd897
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-26 21:38:55 +0500

    update

--- JavaScript/destCity.js
+++ JavaScript/destCity.js
@@ -4,11 +4,19 @@
 */
 
 function destCity(paths) {
-    const cities = paths.flat()
-    const now_city = cities[0]
-    
+    const new_set = new Set()
+    for (const path of paths) {
+        new_set.add(path[1])
+    }   
+    for (const path  of paths) {
+        if(new_set.has(path[0])){
+            new_set.delete(path[0])
+        }
+    }
+    return [...new_set.values()][0]
 }
 
-let paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]
+let paths = [["B", "C"], ["D", "B"], ["C", "A"]]
+// let paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]
 
 console.log(destCity(paths))
