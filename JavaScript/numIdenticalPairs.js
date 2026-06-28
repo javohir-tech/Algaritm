d6a4fc110beae74d679a9c9d2efd917ea2d46399
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-06-29 00:05:57 +0500

    update

--- JavaScript/numIdenticalPairs.js
+++ JavaScript/numIdenticalPairs.js
@@ -6,14 +6,20 @@ var numIdenticalPairs = function (nums) {
     const obj = {}
 
     for (let i = 0; i < nums.length; i++) {
-        if (!nums[i] in obj) {
+        if (!(nums[i] in obj)) {
             obj[nums[i]] = 1
-        }else{
-            obj[nums[i]]++
+        } else {
+            obj[nums[i]] += 1
         }
     }
+    const nums_values = Object.values(obj)
+    let sum = 0
+    for (let i = 0; i < nums_values.length; i++) {
+        let temp_sum = nums_values[i]*(nums_values[i] + 1) / 2   
+        sum += temp_sum - nums_values[i]
+    }
 
-    console.log(obj)
+    return sum
 };
 
 
