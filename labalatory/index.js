7ba0fc2f30d1c43653cca5a5c9bf39a8a2f05273
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-05 00:12:27 +0500

    update

--- labalatory/index.js
+++ labalatory/index.js
@@ -1,18 +1,37 @@
-const promise = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        const success = true
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         const success = true
 
-        if(success){
-            resolve("yaxshi yakunlandi !!!")
-        }else{
-            reject("xatolikga uchradi")
-        }
+//         if(success){
+//             resolve("yaxshi yakunlandi !!!")
+//         }else{
+//             reject("xatolikga uchradi")
+//         }
 
-    })
-})
+//     })
+// })
 
-promise
-.then((res)=>{
-    console.log(res)
-})
-.catch(err=>console.log(err))
+// promise
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch(err=>console.log(err))
+
+// async function salom(){
+//     return "salom"
+// }
+
+// console.log(salom())
+// salom().then(res=>console.log(res))
+
+const getUsers = async ()=>{
+    try {
+        const respose = await fetch("https://jsonplaceholder.typicode.com/users")
+        const res  = await  respose.json()
+        console.log(res)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getUsers()
