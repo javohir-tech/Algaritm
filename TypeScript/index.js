dbec4872a693805504d3a4d47f331ba1b5f92415
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-21 22:04:53 +0500

    update

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let fullName = "John Doe";
+const num = 12;
+// const number: string = "2000";
+const number = 2000;
+const sum = num + number;
+console.log(sum);
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,1 +1,6 @@
-let fullName : string =  "John Doe"
+const num: number = 12;
+// const number: string = "2000";
+const number = 2000
+
+const sum:number = num + number;
+console.log(sum)
