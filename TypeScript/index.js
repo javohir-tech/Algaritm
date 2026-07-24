10ea6d2ccec1490ef4b1a9897cb0f958380372ff
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-24 22:07:26 +0500

    update

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = ["Javohir", 22];
-console.log(person[0]);
-console.log(person[1]);
-// console.log(person[3])
-person.push("Suvonov");
-console.log(person);
-console.log(person.length);
-// console.log(person[2])
+function getInfo() {
+    return ["javohir", 22];
+}
+const logger = getInfo();
+const [fullName, age] = logger;
+console.log(age);
+console.log(fullName);
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,10 +1,9 @@
-const person: [string, number] = ["Javohir", 22];
+function getInfo(): [string, number] {
+  return ["javohir", 22];
+}
 
-console.log(person[0])
-console.log(person[1])
-// console.log(person[3])
+const logger = getInfo();
 
-person.push("Suvonov")
-console.log(person) // [ 'Javohir', 22, 'Suvonov' ]
-console.log(person.length) // 3
-// console.log(person[2])
+const [fullName, age] = logger;
+console.log(age)
+console.log(fullName)
