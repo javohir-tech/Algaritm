4b7b7847fcff182a6cc3ce449b9113f19e2c7d7a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-24 22:46:38 +0500

    update

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,8 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const numbers = [1, 2, 3, 4, 5];
-// numbers.push(6) // error 
-const person = ['Javohir', 22];
-// person.push("Suvonov") // error
-console.log(person);
+var Directions;
+(function (Directions) {
+    Directions["UP"] = "Up";
+    Directions["Down"] = "Down";
+    Directions["Left"] = "Left";
+    Directions["Right"] = "Rigth";
+})(Directions || (Directions = {}));
+// console.log(Directions.UP);
+// console.log(Directions.Down);
+// console.log(Directions.Left);
+// console.log(Directions.Right);
+// const up: Directions = Directions.UP;
+// console.log(up)
+function getInfo(direction) {
+    if (direction === Directions.UP) {
+        return Directions.UP;
+    }
+    else if (direction === Directions.Down) {
+        return Directions.Down;
+    }
+    else if (direction === Directions.Left) {
+        return Directions.Left;
+    }
+    return Directions.Right;
+}
+const res = getInfo(Directions.Down);
+console.log(res);
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,10 +1,30 @@
-const numbers:readonly number[] = [1, 2, 3, 4, 5];
+enum Directions {
+  UP = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Rigth",
+}
 
+// console.log(Directions.UP);
+// console.log(Directions.Down);
+// console.log(Directions.Left);
+// console.log(Directions.Right);
 
-// numbers.push(6) // error 
+// const up: Directions = Directions.UP;
+// console.log(up)
 
-const person : readonly [string , number] = ['Javohir' , 22]
+function getInfo(direction: Directions): string {
+  if (direction === Directions.UP) {
+    return Directions.UP;
+  } else if (direction === Directions.Down) {
+    return Directions.Down;
+  } else if (direction === Directions.Left) {
+    return Directions.Left;
+  }
 
-// person.push("Suvonov") // error
+  return Directions.Right;
+}
 
-console.log(person)
+const res = getInfo(Directions.Down)
+
+console.log(res)
