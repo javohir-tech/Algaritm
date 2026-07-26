ca75157841427ac1f22b6533cdde0a3e05789b23
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-26 17:55:38 +0500

    update

---
+++ TypeScript/UnionTypes/unionTypes.js
@@ -0,0 +1,30 @@
+"use strict";
+// let id :  number | string
+Object.defineProperty(exports, "__esModule", { value: true });
+// id = 12
+// id = "wewww11"
+// console.log(id)
+// TYPE NAROWING
+// function getVal(value : string | number) : string | number{
+//     if(typeof value === 'string'){
+//         return value.toLowerCase()
+//     }else if(typeof value === 'number'){
+//         return value.toFixed(2)
+//     }
+//     return `Unknown type ${value}`
+// }
+// console.log(getVal("javohir"))
+// console.log(getVal(2.345))
+// console.log(getVal(true))
+// function handleError(error: { message: string } | { error: string }) {
+//     if("message" in error){
+//         console.log(error.message.toLowerCase())
+//     }else if("error" in error){
+//         console.log(error.error.toUpperCase())
+//     }
+// }
+// handleError({message : "bu errorda kelgan message"})
+// handleError({error : "bu errorda kelgan error"})
+const data = ["salom", 1, "dunyo", 2];
+console.log(data);
+//# sourceMappingURL=unionTypes.js.map
---
+++ TypeScript/UnionTypes/unionTypes.ts
@@ -0,0 +1,35 @@
+// let id :  number | string
+
+// id = 12
+// id = "wewww11"
+// console.log(id)
+
+// TYPE NAROWING
+
+// function getVal(value : string | number) : string | number{
+//     if(typeof value === 'string'){
+//         return value.toLowerCase()
+//     }else if(typeof value === 'number'){
+//         return value.toFixed(2)
+//     }
+
+//     return `Unknown type ${value}`
+// }
+
+// console.log(getVal("javohir"))
+// console.log(getVal(2.345))
+// console.log(getVal(true))
+
+// function handleError(error: { message: string } | { error: string }) {
+//     if("message" in error){
+//         console.log(error.message.toLowerCase())
+//     }else if("error" in error){
+//         console.log(error.error.toUpperCase())
+//     }
+// }
+
+// handleError({message : "bu errorda kelgan message"})
+// handleError({error : "bu errorda kelgan error"})
+
+const data: (string | number)[] = ["salom", 1, "dunyo", 2];
+console.log(data)
