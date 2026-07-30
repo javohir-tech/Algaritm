ec0a35c58720e1d2ddf8315c438e31b5af9d5037
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-30 21:29:51 +0500

    update

--- TypeScript/class/Access.js
+++ TypeScript/class/Access.js
@@ -5,12 +5,23 @@ class Animal {
     constructor(name) {
         this.name = name;
     }
+    wow() {
+        console.log(this.name);
+    }
 }
 class Dog extends Animal {
     sound() {
-        console.log(`${this.name}`);
+        console.log(`${this.name} 1`);
     }
 }
-const dog = new Dog("Reks");
-dog.sound();
+class SmallDog extends Dog {
+    sound() {
+        super.sound();
+        console.log(this.name + "2");
+    }
+}
+// const dog = new Dog("Reks")
+// dog.sound()
+const smallDog = new SmallDog("reks");
+smallDog.sound();
 //# sourceMappingURL=Access.js.map
--- TypeScript/class/Access.ts
+++ TypeScript/class/Access.ts
@@ -1,20 +1,30 @@
 class Animal {
-    protected name : string
+  protected name: string;
 
-    constructor(name: string){
-        this.name = name
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    wow(){
-        console.log(this.name)
-    }
+  wow() {
+    console.log(this.name);
+  }
 }
 
 class Dog extends Animal {
-    sound(){
-        console.log(`${this.name}`)
-    }
+  sound() {
+    console.log(`${this.name} 1`);
+  }
 }
 
-const dog = new Dog("Reks")
-dog.sound()
+class SmallDog extends Dog {
+  override sound() {
+    super.sound()
+    console.log(this.name + "2");
+  }
+}
+
+// const dog = new Dog("Reks")
+// dog.sound()
+
+const smallDog = new SmallDog("reks");
+smallDog.sound();
