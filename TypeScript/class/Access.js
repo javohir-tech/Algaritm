13209851a73558977f0922bbfbea0c7fc2c08d8d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-07-30 15:59:33 +0500

    access medifier

--- TypeScript/class/Access.js
+++ TypeScript/class/Access.js
@@ -1,34 +1,16 @@
 "use strict";
-// Access medifier
 Object.defineProperty(exports, "__esModule", { value: true });
-// publik - hammaga korishga beradi
-// private - faqat yaratilgan klassi ichida ishlaydi
-// public - faqat meros olingan classda ishlaydi
-class Music {
-    sound;
-    constructor(sound) {
-        this.sound = sound;
-    }
-    showMusicName() {
-        console.log(`${this.sound} mana `);
-    }
-}
-class Author {
+class Animal {
     name;
     constructor(name) {
         this.name = name;
     }
-    showAuthor() {
-        console.log(`${this.name} author`);
+}
+class Dog extends Animal {
+    sound() {
+        console.log(`${this.name}`);
     }
 }
-class Video extends Music {
-    constructor(sound) {
-        super(sound);
-    }
-}
-const author1 = new Author("ozodbek");
-author1.showAuthor();
-const music1 = new Music("sanamlar");
-music1.showMusicName();
+const dog = new Dog("Reks");
+dog.sound();
 //# sourceMappingURL=Access.js.map
--- TypeScript/class/Access.ts
+++ TypeScript/class/Access.ts
@@ -1,47 +1,20 @@
-// Access medifier
+class Animal {
+    protected name : string
 
-// publik - hammaga korishga beradi
-// private - faqat yaratilgan klassi ichida ishlaydi
-// public - faqat meros olingan classda ishlaydi
+    constructor(name: string){
+        this.name = name
+    }
 
-class Music {
-  protected sound: string;
-  protected id: number;
-
-  constructor(sound: string, id: number) {
-    this.sound = sound;
-    this.id = id;
-  }
-
-  showMusicName() {
-    console.log(`${this.sound} mana `);
-  }
+    wow(){
+        console.log(this.name)
+    }
 }
 
-class Author {
-  private name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  showAuthor() {
-    console.log(`${this.name} author`);
-  }
+class Dog extends Animal {
+    sound(){
+        console.log(`${this.name}`)
+    }
 }
 
-class Video extends Music {
-  constructor(sound: string, id: number) {
-    super(sound, id);
-  }
-
-  showVideoMusic() {
-    console.log()
-  }
-}
-
-const author1 = new Author("ozodbek");
-author1.showAuthor();
-
-const music1 = new Music("sanamlar", 12);
-music1.showMusicName();
+const dog = new Dog("Reks")
+dog.sound()
