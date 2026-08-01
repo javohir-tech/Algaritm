11b759b132d3b1f181184f87392941ba1a549982
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-01 12:06:08 +0500

    promise

--- TypeScript/generics/funcGeneric.js
+++ TypeScript/generics/funcGeneric.js
@@ -1,7 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function concatFunc(val1, val2, val3) {
-    return { ...val1, ...val2, ...val3 };
-}
-console.log(concatFunc({ name: "Jonh" }, { role: "Dev" }, { age: 22 }));
 //# sourceMappingURL=funcGeneric.js.map
