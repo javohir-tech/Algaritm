74759d4b46dc63650268b022b93374ac3095a6e7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-02 16:53:56 +0500

    indexed access and conversations

--- TypeScript/Learn/index.ts
+++ TypeScript/Learn/index.ts
@@ -1,35 +1,8 @@
-interface IUser {
-  name: string;
-  age: number;
-  isMerried: boolean;
+type GetPropType<T  , K extends keyof T> = T[K]
+
+type User = {
+    name :  string ;
+    age : number
 }
 
-// indexed access
-type UserName = IUser["name"];
-
-// conversations
-
-type A<T> = T extends boolean ? "yes" | "ha" : "no" | "yoq";
-
-type BooleanType = A<boolean>; // "yes" | "ha"
-type StringType = A<string>; // "no" | "yoq"
-
-function getProporty<T , K extends keyof T>(obj : T , key :K):T[K]{
-    return obj[key]
-}
-
-
-interface  IAdmin {
-    role : "admin" ;
-    access : true
-}
-
-interface IGuest {
-    role : "guest" ;
-    access : "false"
-}
-
-type RoleAccess<T> = T extends {role : "admin"} ? true : false
-
-type AdminType = RoleAccess<IAdmin> // true
-type GuestType = RoleAccess<IGuest> // false
+type UserType = GetPropType<User , "name"> // string
