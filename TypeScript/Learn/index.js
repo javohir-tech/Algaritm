439a201bb45cdbe075ea23e47171820534417a12
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-02 21:16:01 +0500

    practice

--- TypeScript/Learn/index.js
+++ TypeScript/Learn/index.js
@@ -1,3 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+function formValidation(data) {
+    const errors = {};
+    if (!data.email) {
+        errors.email = "Emalnni kiritish shart";
+    }
+    else if (!data.email.includes("@")) {
+        errors.email = "Email is  Invalid";
+    }
+    if (!data.name) {
+        errors.name = "Ism kiritilmagan";
+    }
+    else if (data.name.length < 3) {
+        errors.name = "Ism kamida 3ta belgidan iborat bo'lishi kerak";
+    }
+    if (!data.password) {
+        errors.password = "Password kiritilmagan";
+    }
+    else if (data.password.length < 6) {
+        errors.password = "Password kamida 6 ta belgidan ibboraat bo'lishi kerak";
+    }
+    return errors;
+}
+// test
+const user1 = {
+    name: "Javohir",
+    email: "suvonovjavohir625@gmail.com",
+    password: "1234556",
+};
+console.log(formValidation(user1));
 //# sourceMappingURL=index.js.map
--- TypeScript/Learn/index.ts
+++ TypeScript/Learn/index.ts
@@ -1,8 +1,46 @@
-type GetPropType<T  , K extends keyof T> = T[K]
-
-type User = {
-    name :  string ;
-    age : number
+interface FormDataType {
+  name: string;
+  email: string;
+  password: string;
 }
 
-type UserType = GetPropType<User , "name"> // string
+interface FormErrorType {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
+function formValidation<T extends FormDataType>(data: T): FormErrorType {
+  const errors: FormErrorType = {};
+
+  if (!data.email) {
+    errors.email = "Emalnni kiritish shart";
+  } else if (!data.email.includes("@")) {
+    errors.email = "Email is  Invalid";
+  }
+
+  if (!data.name) {
+    errors.name = "Ism kiritilmagan";
+  } else if (data.name.length < 3) {
+    errors.name = "Ism kamida 3ta belgidan iborat bo'lishi kerak";
+  }
+
+  if (!data.password) {
+    errors.password = "Password kiritilmagan";
+  } else if (data.password.length < 6) {
+    errors.password = "Password kamida 6 ta belgidan ibboraat bo'lishi kerak";
+  }
+
+  return errors;
+}
+
+// test
+const user1: FormDataType = {
+  name: "Javohir",
+  email: "suvonovjavohir625@gmail.com",
+  password: "1234556",
+};
+
+
+console.log(formValidation<FormDataType>(user1))
+
