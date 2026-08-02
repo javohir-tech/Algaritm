65f5300b073970c29eb6a07e0e6a09d9e36d9a41
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-02 22:43:31 +0500

    Extract and Exclude

--- TypeScript/Learn/index.js
+++ TypeScript/Learn/index.js
@@ -1,31 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const product = {
-    name: "iphone",
-    id: 101,
-    price: 800,
-    expect: "chess",
-};
-// 1 .Required
-// expect optional lekin berish kerak sabab Required ishlatilgan
-// typeni hamma proportylarini majburiy qilip qo'yadi qilip qo'yadi 
-function createProduct(product) {
-    return product;
-}
-console.log(createProduct({ name: "samsung", price: 500, id: 102, expect: "chess" }));
-// 2 .Partial 
-// product Partial berilgan shuning uchun istalgan propotyni uztsa boladi 
-// typeni hamma proportylarini optoinal qilip qo'yadi
-function updatedProduct(updateProduct) {
-    const updatedProduct = { ...product, ...updateProduct };
-    console.log(updatedProduct);
-}
-console.log(product);
-updatedProduct({ price: 600 });
-const product2 = {
-    name: "honor",
-    price: 400,
-    id: 303
-};
-// product2.name = "Redmi" //Error 
 //# sourceMappingURL=index.js.map
--- TypeScript/Learn/index.ts
+++ TypeScript/Learn/index.ts
@@ -1,47 +1,16 @@
-interface IProduct {
-name: string;
-  id: number;
-  price: number;
-  expect?: string;
+interface IAnimal {
+  name: string;
+  age: number;
+  sex : string ;
+  high : number
 }
 
-const product: IProduct = {
-  name: "iphone",
-  id: 101,
-  price: 800,
-  expect: "chess",
-};
-
-// 1 .Required
-// expect optional lekin berish kerak sabab Required ishlatilgan
-// typeni hamma proportylarini majburiy qilip qo'yadi qilip qo'yadi 
-
-function createProduct(product: Required<IProduct>): IProduct {
-  return product;
-}
-console.log(createProduct({ name: "samsung", price: 500, id: 102, expect: "chess" }))
-
-// 2 .Partial 
-// product Partial berilgan shuning uchun istalgan propotyni uztsa boladi 
-// typeni hamma proportylarini optoinal qilip qo'yadi
-
-function updatedProduct(updateProduct : Partial<IProduct>){
-    const updatedProduct = {...product , ...updateProduct}
-    console.log(updatedProduct)
+interface IPlan {
+  name: string;
+  age : number;
+  color: string;
 }
 
-console.log(product)
-updatedProduct({price : 600}) 
+type CommonProporties = Extract<keyof IAnimal, keyof IPlan>;
 
-// 3. Readonly
-// Bu hamma proportylarni readonly qilip qo'yadi va o'zgartiripp  bo'lmaydi
-
-type ReadonlyProduct = Readonly<IProduct>
-
-const product2  : ReadonlyProduct = {
-    name  : "honor" , 
-    price : 400 , 
-    id : 303 
-}
-
-// product2.name = "Redmi" //Error 
+type UnCommonProporties  = Exclude<keyof IAnimal , keyof IPlan>
