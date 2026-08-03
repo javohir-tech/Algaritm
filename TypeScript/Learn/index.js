f5045e8cfb0c726511f1d4447a5a89b29d8c2c72
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-03 11:32:37 +0500

    class mixins

--- TypeScript/Learn/index.js
+++ TypeScript/Learn/index.js
@@ -1,8 +1,61 @@
 "use strict";
-// Awaited - asinxron funksiyalarda
 Object.defineProperty(exports, "__esModule", { value: true });
-async function getFetch() {
-    return "Data";
+function CanLog(Base) {
+    return class extends Base {
+        log(message) {
+            console.log(`[LOG] : ${message}`);
+        }
+    };
 }
-const example = 'whats app guy';
+function CanAccess(Base) {
+    return class extends Base {
+        role = "guest";
+        setRole(role) {
+            this.role = role;
+        }
+        canAccess(userRole) {
+            return this.role === userRole;
+        }
+    };
+}
+function CanValidate(Base) {
+    return class extends Base {
+        isValidEmail(email) {
+            return email.includes("@");
+        }
+        isNotEmpty(value) {
+            return value.trim().length > 0;
+        }
+    };
+}
+class User {
+}
+const UserMixin = CanLog(CanAccess(CanValidate(User)));
+class UserServices extends UserMixin {
+    createUser(email, name) {
+        if (!this.isValidEmail(email)) {
+            this.log("email is Invalid");
+            return;
+        }
+        if (!this.isNotEmpty(email)) {
+            this.log("Email  is  empty");
+            return;
+        }
+        if (!this.isNotEmpty(name)) {
+            this.log("Name  is empty");
+            return;
+        }
+        if (!this.canAccess("admin")) {
+            this.log("Sizga bu amaliyotni bajarish taqiqlangan");
+            return;
+        }
+        this.log(`User is created : ${name} ${email}`);
+    }
+}
+const userService = new UserServices();
+const userService1 = new UserServices();
+userService.setRole("user");
+userService.createUser("user@gmail.com", "josh");
+userService1.setRole("admin");
+userService1.createUser("usergmail.com", "doe");
 //# sourceMappingURL=index.js.map
--- TypeScript/Learn/index.ts
+++ TypeScript/Learn/index.ts
@@ -1,9 +1,76 @@
-// Awaited - asinxron funksiyalarda
+type Constructor<T = {}> = new (...args: any[]) => T;
 
-async function getFetch(): Promise<string> {
-  return "Data";
+function CanLog<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    log(message: string) {
+      console.log(`[LOG] : ${message}`);
+    }
+  };
 }
 
-type TypeFetchData = Awaited<ReturnType<typeof getFetch>>;
+function CanAccess<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    role: string = "guest";
 
-const example: TypeFetchData = 'whats app guy'
+    setRole(role: string) {
+      this.role = role;
+    }
+
+    canAccess(userRole: string): boolean {
+      return this.role === userRole;
+    }
+  };
+}
+
+function CanValidate<TBase extends Constructor>(Base: TBase) {
+  return class extends Base {
+    isValidEmail(email: string): boolean {
+      return email.includes("@");
+    }
+
+    isNotEmpty(value: string): boolean {
+      return value.trim().length > 0;
+    }
+  };
+}
+
+class User {}
+
+const UserMixin = CanLog(CanAccess(CanValidate(User)));
+
+class UserServices extends UserMixin {
+  createUser(email: string, name: string) {
+    if (!this.isValidEmail(email)) {
+      this.log("email is Invalid");
+      return;
+    }
+
+    if (!this.isNotEmpty(email)) {
+      this.log("Email  is  empty");
+      return
+    }
+
+    if(!this.isNotEmpty(name)){
+      this.log("Name  is empty")
+      return
+    }
+
+    if(!this.canAccess("admin")){
+      this.log("Sizga bu amaliyotni bajarish taqiqlangan")
+      return
+    }
+
+    this.log(`User is created : ${name} ${email}`)
+  }
+}
+
+const userService =  new UserServices()
+const userService1 =  new UserServices()
+
+userService.setRole("user")
+userService.createUser("user@gmail.com" ,  "josh")
+
+userService1.setRole("admin")
+userService1.createUser("usergmail.com" , "doe")
+
+
