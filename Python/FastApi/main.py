167b24fc9b203c1a23cdfa6df1164a95ab6480d2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-10 00:20:36 +0500

    update

--- Python/FastApi/main.py
+++ Python/FastApi/main.py
@@ -1,13 +1,38 @@
 from config import settings
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from auth_routers import auth_routes
 from product_routes import product_routes
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(product_routes)
 app.include_router(auth_routes)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    first = errors[0]
+
+    field = first["loc"][-1]
+    msg = first["msg"]
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+        content={"detail": f"{field} : {msg}"},
+    )
 
 
 @app.get("/")
