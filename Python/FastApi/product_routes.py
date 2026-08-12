244db1fcab509143033313ff965ae88d10a4da88
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-12 14:48:23 +0500

    feat: add delete and get product endpoints
    - DELETE /delete/{product_id} — only owner can delete

--- Python/FastApi/product_routes.py
+++ Python/FastApi/product_routes.py
@@ -75,6 +75,24 @@ async def getAll(db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{e}")
 
 
+@product_routes.get("/my_products")
+async def getMyProduct(
+    db: AsyncSession = Depends(get_db), current_user: str = Depends(verify)
+):
+
+    result = await db.execute(
+        select(User)
+        .options(selectinload(User.products))
+        .filter(User.username == current_user)
+    )
+
+    user = result.scalar_one_or_none()
+
+    products = user.products
+
+    return {"success": True, "message": "Successfully fetched", "data": products}
+
+
 @product_routes.get("/{product_id}", status_code=status.HTTP_200_OK)
 async def getProductById(product_id: int, db: AsyncSession = Depends(get_db)):
     try:
@@ -151,3 +169,41 @@ async def updateProduct(
             "user_id": product.user_id,
         },
     }
+
+
+@product_routes.delete("/delete/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def deleteProduct(
+    product_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: str = Depends(verify),
+):
+    product_result = await db.execute(
+        select(Product)
+        .options(selectinload(Product.user))
+        .filter(Product.id == product_id)
+    )
+
+    product = product_result.scalar_one_or_none()
+
+    if product is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Product not  found"
+        )
+
+    if product.user.username != current_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not allowed to delete this product",
+        )
+
+    try:
+        await db.delete(product)
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Failed to update product due to a database constraint",
+        )
+
+    return {"success": True, "message": "Successfully deleted"}
